239e43aae5832ed19c6dc731ac3d120018d65ae0
Guk Jin Han <rnrwls2482@naver.com>
2024-02-26 01:35:22 +0900

    연습

---
+++ Practice/p01_practice.py
@@ -0,0 +1,20 @@
+a = 10*18**2+2*11
+print(a)
+
+a = 14
+b = 3
+print(a // b)
+print(a % b)
+
+multiline = '''Life is too short
+You need python
+'''
+print(multiline)
+
+print("="*50)
+print("My Program")
+print("="*50)
+
+a = 'You need Python'
+print(len(a))
+print(a[0])
